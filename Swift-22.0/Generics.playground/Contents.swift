d9a2b93c79a512c6a7354c817cfa81c83c0447d8
Juan Sicilia <siciliajuan@gmail.com>
2017-06-12 22:04:01 +0200

    Generic finished

--- Swift-22.0/Generics.playground/Contents.swift
+++ Swift-22.0/Generics.playground/Contents.swift
@@ -2,7 +2,7 @@
 
 import Cocoa
 
-struct Stack<Element> {
+struct Stack<Element>: SequenceType {
     var items = [Element]()
     
     mutating func push(_ newItem: Element) {
@@ -22,6 +22,10 @@ struct Stack<Element> {
             mappedItems.append(f(item))
         }
         return Stack<U>(items: mappedItems)
+    }
+    
+    func generate() -> StackGenerator<Element> {
+        return StackGenerator(stack: self)
     }
 }
 
@@ -73,8 +77,64 @@ func checkIfEqual<T: Equatable>(_ first: T, _ second: T) -> Bool {
     return first == second
 }
 
+print(checkIfEqual(1, 1))
+print(checkIfEqual("a string", "a string"))
+print(checkIfEqual("a string", "a diferent string"))
 
 
+func checkIfDescriptionsMatch<T: CustomStringConvertible, U: CustomStringConvertible> (_ first: T, _ second: U) -> Bool {
+    return first.description == second.description
+}
+
+print(checkIfDescriptionsMatch(Int(1), UInt(1)))
+print(checkIfDescriptionsMatch(1, 0.1))
+print(checkIfDescriptionsMatch(Float(1.0), Double(1.0)))
+
+
+
+// Associated Type
+
+
+// Here we use associatedType to create a generic that gonna be reset by the 
+// Object that implements this protocol
+protocol GeneratorType {
+    associatedtype Element
+    
+    mutating func next() -> Element?
+}
+
+struct StackGenerator<T>: GeneratorType {
+    // It isn't needed as works by inference
+    //typealias Element = T
+    
+    var stack: Stack<T>
+    
+    mutating func next() -> T? {
+        return stack.pop()
+    }
+}
+
+var myStack = Stack<Int>()
+myStack.push(10)
+myStack.push(20)
+myStack.push(30)
+var myStackGenerator = StackGenerator(stack: myStack)
+while let value = myStackGenerator.next() {
+    print("got \(value)")
+}
+
+
+
+// Here we use associated type to force the return of the function
+// generate() to be something that implements GeneratorType Protocol
+protocol SequenceType {
+    associatedtype Generator: GeneratorType
+    func generate() -> Generator
+}
+
+for value in myStack {
+    print("for-on loop: got \(value)")
+}
 
 
 
